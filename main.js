c10b30208750c7817ace894e7a5ed0811ed43973
adiwajshing <95894400+hardianto02@users.noreply.github.com>
2022-01-14 07:10:56 +0000

    pe

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 const PDFDocument = require('pdfkit');
 const sizes = require("./sizes.json")
-
+//const fetch = require("node-fetch")
+function getBuffer(url) {
+	const response = await fetch(url)
+	const arr = await response.arrayBuffer()
+	const buffer =  Buffer.from(arr)
+	return buffer
+}
 module.exports = (pages, size) => {
     const doc = new PDFDocument({ margin: 0, size });
 	
 	for (let index = 0; index < pages.length; index++) {
-		doc.image(pages[index], 0, 0, { fit: sizes[size], align: 'center', valign: 'center' });
+		const image = getBuffer(pages[index])
+		doc.image(image, 0, 0, { fit: sizes[size], align: 'center', valign: 'center' });
 
 		if (pages.length != index + 1) doc.addPage();
 	}
