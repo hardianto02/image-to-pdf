12831783aa395f3deef3f34aa1472b3421451769
adiwajshing <95894400+hardianto02@users.noreply.github.com>
2022-01-14 07:58:02 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,22 @@
+const { page } = require('pdfkit');
 const PDFDocument = require('pdfkit');
 const sizes = require("./sizes.json")
 //const fetch = require("node-fetch")
- async function getBuffer(url) {
+  function getBuffer(url) {
+return new Promise(async(resolve, reject) => {
 	const response = await fetch(url)
 	const arr = await response.arrayBuffer()
 	const buffer = Buffer.from(arr)
-	return buffer
-}
+	resolve(buffer)
+	reject(null)
+})
+  }
 const topdf = (pages, size) => {
     const doc = new PDFDocument({ margin: 0, size });
-	
 	for (let index = 0; index < pages.length; index++) {
 		console.log(pages[index])
-		const image = (getBuffer(pages[index])).then(b => b)
-		doc.image(image, 0, 0, { fit: sizes[size], align: 'center', valign: 'center' });
+		const image = async () => await getBuffer(pages[index])
+		doc.image(image(), 0, 0, { fit: sizes[size], align: 'center', valign: 'center' });
 
 		if (pages.length != index + 1) doc.addPage();
 	}
